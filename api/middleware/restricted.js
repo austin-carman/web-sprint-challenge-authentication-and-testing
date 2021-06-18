6a90ab033738a8d523702839d7d4dfe152a63784
Austin Carman <abcarman12@gmail.com>
2021-06-18 12:13:09 -0600

    built restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const { JWT_SECRET } = require('../../secret/index');
+const jwt = require('jsonwebtoken');
+
+
 module.exports = (req, res, next) => {
-  
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'token invalid' })
+      } else {
+        req.decodedJwt = decoded;
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'token required' });
+  }
   next();
   /*
     IMPLEMENT
